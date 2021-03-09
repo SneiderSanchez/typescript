a0473dfd431b5c97befcb22db7d53ae3ea093a5b
SneiderSanchez <sneiderandres09@hotmail.com>
2021-03-09 18:55:58 -0500

    feat: basic types

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log("Hey Platzi world!");
+// console.log("Hey Platzi world!");
+// Number
+// Explicit
+var phone;
+phone = 123456789;
+// phone = "hola"; // Error
+// implicit
+var phoneNumber = 123456789;
+// phoneNumber = true; // Error
+var hex = 0xf;
+var binary = 10;
+var octal = 484;
+// Type: Boolean
+// Explicit
+var isPro;
+isPro = false;
+// isPro = 10; // Error
+//Implicit
+var isUserPro = false;
+isUserPro = true;
+// isUserPro = 10; // Error
+// Type: string
+var userName = "sneider";
+userName = "pedro";
+// userName = false; // Error
+// Template String
+var userInfo;
+userInfo = "I'm " + userName + " and my number is " + phone + "!";
+console.log(userInfo);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
-console.log("Hey Platzi world!");
+// console.log("Hey Platzi world!");
+
+// Number
+// Explicit
+
+let phone: number;
+phone = 123456789;
+// phone = "hola"; // Error
+
+// implicit
+let phoneNumber = 123456789;
+// phoneNumber = true; // Error
+
+let hex: number = 0xf;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// Type: Boolean
+
+// Explicit
+let isPro: boolean;
+isPro = false;
+// isPro = 10; // Error
+
+//Implicit
+let isUserPro = false;
+isUserPro = true;
+// isUserPro = 10; // Error
+
+// Type: string
+
+let userName: string = "sneider";
+userName = "pedro";
+// userName = false; // Error
+
+// Template String
+let userInfo: string;
+userInfo = `I'm ${userName} and my number is ${phone}!`;
+
+console.log(userInfo);
