e1ff184c62351dbef28680cf652c263e38416cfe
SneiderSanchez <sneiderandres09@hotmail.com>
2021-03-10 18:53:37 -0500

    feat: strictnullcheck and null/undefined types

---
+++ dist/type-null-undefined.js
@@ -0,0 +1,15 @@
+// Null
+var nullVariable;
+nullVariable = null;
+//nullVariable = 1; //Error
+var otherVariable = null;
+otherVariable = "test";
+//  Undefined
+var undefinedVariable = undefined;
+//undefinedVariable = "test"; // Error
+var otherUndefined = undefined;
+otherUndefined = "hey";
+// --strictNullChecks
+var albunName;
+/* albunName = null;
+albunName = undefined; */
---
+++ src/type-null-undefined.ts
@@ -0,0 +1,22 @@
+// Null
+
+let nullVariable: null;
+nullVariable = null;
+//nullVariable = 1; //Error
+
+let otherVariable = null;
+otherVariable = "test";
+
+//  Undefined
+
+let undefinedVariable: undefined = undefined;
+//undefinedVariable = "test"; // Error
+
+let otherUndefined = undefined;
+otherUndefined = "hey";
+
+// --strictNullChecks
+let albunName: string;
+
+/* albunName = null;
+albunName = undefined; */
