435eb6ece57e410967b5f8a595506e73b8ff3f35
SneiderSanchez <sneiderandres09@hotmail.com>
2021-03-11 18:55:11 -0500

    feat: Array type

---
+++ dist/type-array-tuple.js
@@ -0,0 +1,14 @@
+"use strict";
+// []
+// Explicit
+var users;
+users = ["test", "test2", "test3"];
+// users = [1, true, "hey"]; // Error
+// Implicit
+var otherUsers = ["test", "test2", "test3"];
+// otherUsers = [1, true, "hey"]; // Error
+// Array<Type>
+var pictureTitles;
+pictureTitles = ["title", "title2", "title3"];
+// Values in array
+console.log("firstUser", users[0]);
---
+++ src/type-array-tuple.ts
@@ -0,0 +1,18 @@
+// []
+
+// Explicit
+let users: string[];
+users = ["test", "test2", "test3"];
+// users = [1, true, "hey"]; // Error
+// Implicit
+
+let otherUsers = ["test", "test2", "test3"];
+// otherUsers = [1, true, "hey"]; // Error
+
+// Array<Type>
+
+let pictureTitles: Array<string>;
+pictureTitles = ["title", "title2", "title3"];
+
+// Values in array
+console.log("firstUser", users[0]);
